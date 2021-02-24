1f35d5743d40c4c8c8004e93c6f64cdb398c54f1
Lee Briggs <lee@leebriggs.co.uk>
2021-02-23 21:31:39 -0800

    Revert "update output methods"
    This reverts commit d416dd5119dc3ff47fcd51747a25db18e6cfb15a.

--- cmd/ploy/up/cli.go
+++ cmd/ploy/up/cli.go
@@ -101,9 +101,9 @@ func Command() *cobra.Command {
 			} else {
 
 				actionSpinner := utils.TerminalSpinner{
-					SpinnerText:   fmt.Sprintf("Creating ploy deployment: %s", name),
-					CompletedText: fmt.Sprintf("✅ Ploy deployment created: %s", name),
-					FailureText:   fmt.Sprintf("❌ Error creating Ploy deployment: %s", name),
+					SpinnerText:   "Creating ploy deployment",
+					CompletedText: "✅ Ploy deployment created.",
+					FailureText:   "❌ Error creating Ploy deployment.",
 				}
 
 				actionSpinner.Create()
